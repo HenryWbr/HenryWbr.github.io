6e5cc66de50017f2c07d587310a3be0a30f6e05e
HenryWbr <wambrauwhenry@gmail.com>
2022-04-03 02:08:30 +0900

    initial commit

---
+++ beranda.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="style/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>My Portofolio</title>
+</head>
+
+<body>
+
+    <!--Navbar-->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand fst-italic" id="deskcard">Henry W Wambrauw</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" id="deskcard">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#project" id="deskcard">Project</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#kontak" id="deskcard">Kontak</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    <a href="" class="btn btn-outline-secondary" id="deskcard">Login</a>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+
+    <!--Head-->
+    <div class="bg-dark text-light text-center" id="head">
+        <img src="img/avatar.png" class="mx-auto d-block w-25" alt="...">
+
+        <div class="container">
+            <h1 class="" id="respfonthead">Henry William Wambrauw</h1>
+            <h3 class="display-7" id="desk">
+                Web Design | College Student
+            </h3>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1" d="M0,192L8.6,165.3C17.1,139,34,85,51,58.7C68.6,32,86,32,103,42.7C120,53,137,75,154,101.3C171.4,128,189,160,206,192C222.9,224,240,256,257,245.3C274.3,235,291,181,309,149.3C325.7,117,343,107,360,90.7C377.1,75,394,53,411,53.3C428.6,53,446,75,463,101.3C480,128,497,160,514,170.7C531.4,181,549,171,566,176C582.9,181,600,203,617,181.3C634.3,160,651,96,669,80C685.7,64,703,96,720,117.3C737.1,139,754,149,771,133.3C788.6,117,806,75,823,64C840,53,857,75,874,117.3C891.4,160,909,224,926,250.7C942.9,277,960,267,977,272C994.3,277,1011,299,1029,288C1045.7,277,1063,235,1080,208C1097.1,181,1114,171,1131,165.3C1148.6,160,1166,160,1183,138.7C1200,117,1217,75,1234,80C1251.4,85,1269,139,1286,181.3C1302.9,224,1320,256,1337,224C1354.3,192,1371,96,1389,53.3C1405.7,11,1423,21,1431,26.7L1440,32L1440,320L1431.4,320C1422.9,320,1406,320,1389,320C1371.4,320,1354,320,1337,320C1320,320,1303,320,1286,320C1268.6,320,1251,320,1234,320C1217.1,320,1200,320,1183,320C1165.7,320,1149,320,1131,320C1114.3,320,1097,320,1080,320C1062.9,320,1046,320,1029,320C1011.4,320,994,320,977,320C960,320,943,320,926,320C908.6,320,891,320,874,320C857.1,320,840,320,823,320C805.7,320,789,320,771,320C754.3,320,737,320,720,320C702.9,320,686,320,669,320C651.4,320,634,320,617,320C600,320,583,320,566,320C548.6,320,531,320,514,320C497.1,320,480,320,463,320C445.7,320,429,320,411,320C394.3,320,377,320,360,320C342.9,320,326,320,309,320C291.4,320,274,320,257,320C240,320,223,320,206,320C188.6,320,171,320,154,320C137.1,320,120,320,103,320C85.7,320,69,320,51,320C34.3,320,17,320,9,320L0,320Z"></path>
+        </svg>
+    </div>
+
+
+
+
+
+    <!--Project-->
+    <div class="container-fluid" id="project">
+        <h1 class="text-center display-3" id="respfonthead">Project</h1> <br>
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/IMG.jpg" class="card-img-top mx-auto w-75" alt="img/IMG.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title" id="headcard">Card title</h5>
+                        <p class="card-text" id="deskcard">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-outline-secondary" id="deskcard">Lihat</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/IMG.jpg" class="card-img-top mx-auto w-75" alt="img/IMG.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title" id="headcard">Card title</h5>
+                        <p class="card-text" id="deskcard">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-outline-secondary" id="deskcard">Lihat</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/IMG.jpg" class="card-img-top mx-auto w-75" alt="img/IMG.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title" id="headcard">Card title</h5>
+                        <p class="card-text" id="deskcard">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-outline-secondary" id="deskcard">Lihat</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/IMG.jpg" class="card-img-top mx-auto w-75" alt="img/IMG.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title" id="headcard">Card title</h5>
+                        <p class="card-text" id="deskcard">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-outline-secondary" id="deskcard">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+    <!--Kontak-->
+    <div class="bg-dark text-light" id="kontak">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1" d="M0,192L8.6,165.3C17.1,139,34,85,51,58.7C68.6,32,86,32,103,42.7C120,53,137,75,154,101.3C171.4,128,189,160,206,192C222.9,224,240,256,257,245.3C274.3,235,291,181,309,149.3C325.7,117,343,107,360,90.7C377.1,75,394,53,411,53.3C428.6,53,446,75,463,101.3C480,128,497,160,514,170.7C531.4,181,549,171,566,176C582.9,181,600,203,617,181.3C634.3,160,651,96,669,80C685.7,64,703,96,720,117.3C737.1,139,754,149,771,133.3C788.6,117,806,75,823,64C840,53,857,75,874,117.3C891.4,160,909,224,926,250.7C942.9,277,960,267,977,272C994.3,277,1011,299,1029,288C1045.7,277,1063,235,1080,208C1097.1,181,1114,171,1131,165.3C1148.6,160,1166,160,1183,138.7C1200,117,1217,75,1234,80C1251.4,85,1269,139,1286,181.3C1302.9,224,1320,256,1337,224C1354.3,192,1371,96,1389,53.3C1405.7,11,1423,21,1431,26.7L1440,32L1440,0L1431.4,0C1422.9,0,1406,0,1389,0C1371.4,0,1354,0,1337,0C1320,0,1303,0,1286,0C1268.6,0,1251,0,1234,0C1217.1,0,1200,0,1183,0C1165.7,0,1149,0,1131,0C1114.3,0,1097,0,1080,0C1062.9,0,1046,0,1029,0C1011.4,0,994,0,977,0C960,0,943,0,926,0C908.6,0,891,0,874,0C857.1,0,840,0,823,0C805.7,0,789,0,771,0C754.3,0,737,0,720,0C702.9,0,686,0,669,0C651.4,0,634,0,617,0C600,0,583,0,566,0C548.6,0,531,0,514,0C497.1,0,480,0,463,0C445.7,0,429,0,411,0C394.3,0,377,0,360,0C342.9,0,326,0,309,0C291.4,0,274,0,257,0C240,0,223,0,206,0C188.6,0,171,0,154,0C137.1,0,120,0,103,0C85.7,0,69,0,51,0C34.3,0,17,0,9,0L0,0Z"></path>
+        </svg>
+        <h1 class="text-center display-3" id="respfonthead">Kontak</h1> <br>
+        <div class="container text-center pb-5">
+            <!-- <form action="sendWA.php" method="POST" target="_blank"> -->
+            <!-- <a href="sendWA.php" name="submit" class="btn btn-primary">Kirim</a> -->
+            <!-- <button type="submit" name="submit" class="btn btn-outline-primary" id="deskcard">Kirim</button> -->
+            <!-- </form> -->
+
+            <a href="sendWA.php" title="WhatsApp">
+                <img src="img/WA.png" width="150" alt="Logo Whatsapp" />
+            </a>
+            <a href="img/barcode IG.jpeg" title="Instagram">
+                <img src="img/IG.png" width="110" alt="Logo Instagram" />
+            </a>
+
+            <br><br><br><br>
+            <p class="text-center" id="deskcard">Created with Love &#x1F60D; by Henry William Wambrauw@2022</p>
+        </div>
+
+    </div>
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ sendWA.php
@@ -0,0 +1,3 @@
+<?php
+
+header("location:https://api.whatsapp.com/send?phone=+6282162925156&text=Hallo%20Kakak,%0Dsaya%20ingin%20bertanya%20sedikit%20tentang%20website%20(^_^)");
